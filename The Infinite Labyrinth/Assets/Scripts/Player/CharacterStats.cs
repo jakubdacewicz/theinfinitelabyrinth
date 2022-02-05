1159019661d940963e1310b55778a3d51c145f0f
Jakub <99081886+jakubdacewicz@users.noreply.github.com>
2022-02-05 21:42:56 +0100

    Rozbudowa CharacterStats
    Rozbudowa punktów życia

--- The Infinite Labyrinth/Assets/Scripts/Player/CharacterStats.cs
+++ The Infinite Labyrinth/Assets/Scripts/Player/CharacterStats.cs
@@ -2,5 +2,36 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat health;
+    private float _maxHealth;
+    public float _currentHealth;
+
+    public void SetMaxHealth(float value)
+    {
+        _maxHealth += value;    
+    }
+
+    public void Heal(float value)
+    {
+        if (_currentHealth + value > _maxHealth)
+        {
+            value = _maxHealth - _currentHealth;
+        }
+        _currentHealth += value;
+    }
+
+    public void TakeDamage(float value)
+    {
+        _currentHealth -= value;
+        Debug.Log(transform.name + "took " + value + " damage.");
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player died!");
+    }
 }
